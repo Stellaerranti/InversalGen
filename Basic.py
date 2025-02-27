b829f093a3e9665a20e6aaf16912e9a1d56b181c
Stellaerranti <ushakovda@my.msu.ru>
2025-02-27 14:35:23 +0300

    Mean gap and rate

--- Basic.py
+++ Basic.py
@@ -1,41 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def simulate_geomagnetic_reversals(rate, time_span):
-
+def simulate_geomagnetic_reversals(rate_per_myr, time_span_myr, min_gap_years=500):
     reversal_times = []
     current_time = 0
+    min_gap_myr = min_gap_years / 1e6  # Convert years to million years
     
-    while current_time < time_span:
-        wait_time = np.random.exponential(1 / rate)  # Poisson process: Exponential waiting times
+    while current_time < time_span_myr:
+        wait_time = np.random.exponential(1 / rate_per_myr)  # Poisson process: Exponential waiting times
+        if wait_time < min_gap_myr:
+            continue  # Ensure minimum gap is maintained
         current_time += wait_time
-        if current_time < time_span:
+        if current_time < time_span_myr:
             reversal_times.append(current_time)
     
     return np.array(reversal_times)
 
-def plot_magnetochron(reversal_times, time_span):
+def plot_magnetochron(reversal_times, time_span_myr):
+
     fig, ax = plt.subplots(figsize=(10, 2))
     
-    # Initial state (assume normal polarity at the start)
-    times = np.concatenate(([0], reversal_times, [time_span]))
+    times = np.concatenate(([0], reversal_times, [time_span_myr]))
     colors = ["black" if i % 2 == 0 else "white" for i in range(len(times) - 1)]
     
     for i in range(len(times) - 1):
         ax.fill_betweenx([0, 1], times[i], times[i+1], color=colors[i])
     
-    ax.set_xlim(0, time_span)
+    ax.set_xlim(0, time_span_myr)
     ax.set_yticks([])
     ax.set_xlabel("Time (Million Years)")
-    ax.set_title("Simulated Geomagnetic Reversals (Magnetochron Format)")
+    ax.set_title("Simulated Geomagnetic Reversals")
     plt.show()
 
-# Parameters
-mean_reversal_rate = 2  # Average reversals per million years (example value)
-time_span = 10  # Total time span in million years
+# User-defined parameters
+time_span_myr = 1  # Total time in million years
+mean_reversal_rate = 5  # Reversals per million years
+min_gap_years = 20000  # Minimum gap between reversals in years
 
 # Run simulation
-reversal_times = simulate_geomagnetic_reversals(mean_reversal_rate, time_span)
+reversal_times = simulate_geomagnetic_reversals(mean_reversal_rate, time_span_myr, min_gap_years)
 
 # Plot results
-plot_magnetochron(reversal_times, time_span)
+plot_magnetochron(reversal_times, time_span_myr)
